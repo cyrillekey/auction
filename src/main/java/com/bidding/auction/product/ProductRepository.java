ef092c9c990d57e2fdd4fcc2a3f405ea63bec660
cyrille <cyrilleotieno7@gmail.com>
2021-12-26 13:45:07 +0300

    search fix

--- src/main/java/com/bidding/auction/product/ProductRepository.java
+++ src/main/java/com/bidding/auction/product/ProductRepository.java
@@ -1,14 +1,14 @@
 package com.bidding.auction.product;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Integer>{
-    @Query(value = "SELECT * from product where pname LIKE %in%" ,nativeQuery = true)
-    List<Product> findByPnameContaining(String pname);
+    @Query(value = "SELECT * from product where pname LIKE %:pname%" ,nativeQuery = true)
+    List<Product> findByPnameContaining ( @Param("pname")String pname);
 }
